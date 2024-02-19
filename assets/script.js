4c81b7028b1e8c50e2f8b436ff28849d0b439abc
Ranjan Aryal <rnaryal@gwmail.gwu.edu>
2024-02-19 18:57:45 -0500

    file copied

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var firstNum = parseInt(console.log (window.prompt("enter a number")));
+var secondNum = parseInt(console.log(window.prompt("Enter a second number")));
+
+sum = (firstNum + secondNum);
+console.log(sum);
+
+// parseInt firstNum;
+// parseInt secondNume;
+
+// sum = (firstNum + secondNum);
+// alert (sum);
+
