40f3c2621fd377f5ec707198545d9c7b46663aa5
Ranjan Aryal <rnaryal@gwmail.gwu.edu>
2024-02-19 23:12:45 -0500

    couldn't fix parseInt in VS code

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,44 @@
-var firstNum = console.log (window.prompt("enter a number"));
+var firstNum = console.log (window.prompt("Enter a first number"));
 var secondNum = console.log(window.prompt("Enter a second number"));
 
-// Checking if else statement for a while
+// parseInt test
+a = parseInt(firstNum);
+b = parseInt(secondNum);
+
+totalAdd = a + b;
+window.alert(totalAdd); 
+// Not success in converting user assigned inf to number :(
+
+
+// Checking if statement for a while, not a part of calculater until now
 if (firstNum > 50) {
     window.alert ("Your first number is greater than 50");
 } else {
     window.alert("Your first number is equal or smaller than 50 because computer assumes the entered number as string");
 }
 
-sum = firstNum + secondNum;
-// window.alert(sum);
+// checking else if statement, still not part of final app
+testNum = 25
+if (testNum > 20) {
+    window.alert("Test number is greater than 20");
+} else if (testNum == 20) {
+    window.alert("Test number is equal to 20");
+} else window.alert("Test number is smaller than 20")
 
-// parseInt firstNum;
-// parseInt secondNume;
 
-// sum = (firstNum + secondNum);
-// alert (sum);
 
-console.log("hello world");
+// Some extra maths as console.log doesn't keep record for future
+x = 3.14159265
+y = 5
+z = 7
+
+window.alert(Math.max(x, y, z));
+console.log(Math.round(x));
+console.log(Math.floor(x)); 
+console.log(Math.ceil(x));
+console.log(Math.pow(x,2));
+console.log(Math.sqrt(x));
+console.log(Math.abs(x));
+console.log(Math.max(x, y, z));
+console.log(Math.min(x, y, z));
+
